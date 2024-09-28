11431e98aa8a4c1718daa9bd06c65a0a15e7d594
Joshua Lytle <joshua.lytle@uky.edu>
2024-09-27 23:34:42 -0400

    added base Tile class

---
+++ include/game_board/tile/base_tile.hpp
@@ -0,0 +1,34 @@
+// =======================================================
+// Description: This header file contains the declaration
+//              of the Tile base class. This will define
+//              the base members for each of the game
+//              of the game tiles.
+// =======================================================
+
+#ifndef GAME_BOARD_TILE_BASE_H
+#define GAME_BOARD_TILE_BASE_H
+
+namespace game_board::tile
+{
+enum class ColorSide{
+  kRed,
+  kGreen,
+  kBlue,
+  kYellow,
+  kNone
+};
+
+class Tile
+{
+ public:
+  /// @brief Call to determine if a tile can be whoopsed
+  /// @return True if a game piece can be whoopsed
+  virtual bool CanWhoops() { return false; }
+
+ private:
+  int index_{-1};
+  ColorSide tile_color_{ColorSide::kNone};
+  ColorSide color_occupied_{ColorSide::kNone};
+};
+} // namespace game_board::tile
+#endif // GAME_BOARD_TILE_BASE_H
