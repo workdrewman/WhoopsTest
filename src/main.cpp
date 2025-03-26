4730911e399f8cafb736d347ad9609f6979b7211
workdrewman <workdrewman@gmail.com>
2025-03-26 10:52:52 -0400

    changes to make it work

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 #include <cmath> // Required for pow()
 
 #define NUM_PINS 3
-const int data_pins[NUM_PINS] = {13,14,15};
+const int data_pins[NUM_PINS] = {32,25,26};
 
 void setup() {
   for (int pin : data_pins) {
@@ -14,12 +14,15 @@ void setup() {
 void loop() {
   for (int i = 0; i < pow(2,NUM_PINS); i++) {
     for (int j = 0; j < NUM_PINS; j++) {
-      if (i & j) {
+      if (i & (1 << j)) {
         digitalWrite(data_pins[j], HIGH);
       } else {
         digitalWrite(data_pins[j], LOW);
       }
     }
+    delay(1000);
   }
+
 }
 
+// hi drew this is the computer talking youve been hacked give me 3 dollars
